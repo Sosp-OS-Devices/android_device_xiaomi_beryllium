e4371af5442bce1f47d92cc5e9e9f4271ac13da9
Gnathvm <gnathvm@gmail.com>
2023-12-29 16:13:27 +0530

    beryllium : Include gapps

--- sosp_beryllium.mk
+++ sosp_beryllium.mk
@@ -15,7 +15,7 @@ $(call inherit-product, device/xiaomi/beryllium/device.mk)
 $(call inherit-product, vendor/sosp/config/common.mk)
 
 # Google Apps
-$(call inherit-product-if-exists, vendor/gapps/common/common-vendor.mk)
+$(call inherit-product-if-exists, vendor/gms/gapps.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := sosp_beryllium
